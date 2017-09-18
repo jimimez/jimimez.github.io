ad0e5ef41e554cc73dbb711f705b3036b0c1fdd9
GeoComputers-PC\Geo Computers <jimimez@gmail.com>
2017-09-18 19:50:05 +0400

    added slider function

--- theham/js/main.js
+++ theham/js/main.js
@@ -1,3 +1,47 @@
-$(".navlink").hover(function(){
-	$(this).addClass("border");
-})
+var elementWidth, childCount;
+var activeIndex = 0;
+var speed = 300;
+
+$('doument').ready(function(){
+
+	elementWidth = $('.slide').width();
+	childCount = $('.slide').length;
+	$('.slider-button-right').on('click', function(){
+		goToRight();
+	})
+	$('.slider-button-left').on('click', function(){
+		goToLeft();
+
+	})
+
+	
+});
+
+	
+	function goToSlide(index){
+		if((index > childCount - 1)){
+			index = 0;
+		}
+		if(index < 0){
+			index =  childCount -1
+		}
+
+		$(".slider-container").animate({left : - (index * elementWidth)}, speed, function(){
+			activeIndex = index;
+		
+		});
+	}
+
+	function goToLeft(){
+		goToSlide(activeIndex - 1);
+	}
+
+	function goToRight(){
+		goToSlide(activeIndex + 1);
+	}
+
+	
+
+
+
+	
