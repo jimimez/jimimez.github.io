1ab47609d2727de1e9e6d3b62016271aa0ad5e16
GeoComputers-PC\Geo Computers <jimimez@gmail.com>
2017-11-12 23:57:07 +0400

    no message

--- test/js/main.js
+++ test/js/main.js
@@ -1,4 +1,13 @@
 function burger() {
     var element = document.getElementById("header");
     element.classList.toggle("menu-active");
+
+    var element2 = document.getElementById("line2");
+    element2.classList.toggle("hide");
+
+    var element3 = document.getElementById("line1");
+    element3.classList.toggle("rotate1");
+
+    var element4 = document.getElementById("line3");
+    element4.classList.toggle("rotate2");
 }
